37abb6c3cf30d03fb48afeb167ce445c1e955486
Cristian Bermudez Martinez <cristianbm2095@gmail.com>
2024-02-05 20:07:00 +0100

    Resolved exercise 30

---
+++ Pages/30-grains.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+enum GrainsError: Error {
+  case inputTooLow
+  case inputTooHigh
+}
+
+struct Grains {
+  static func square(_ num: Int) throws -> UInt64 {
+    if num < 1 {
+      throw GrainsError.inputTooLow
+    }
+    if num > 64 {
+      throw GrainsError.inputTooHigh
+    }
+    let result = pow(2, Double(num - 1))
+    return UInt64(result)
+  }
+  
+  static var total: UInt64 {
+    var totalGrains: UInt64 = 0
+    var currentGrains: UInt64 = 1
+    for _ in 1...63 {
+      totalGrains += currentGrains
+      currentGrains *= 2
+    }
+    totalGrains += currentGrains
+    
+    return totalGrains
+  }
+}
+
+Grains.square(4)
+Grains.square(32)
+Grains.total
